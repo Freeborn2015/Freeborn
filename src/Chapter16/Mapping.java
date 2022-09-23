e57ea4a2a6c2818329d8317359bca5cd6315bf47
Freeborn <freeborngas@gmail.com>
2022-09-23 15:25:43 +0100

    java

--- src/Chapter16/Mapping.java
+++ src/Chapter16/Mapping.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class Main {
+public class Mapping {
+
     public static void main(String[] args) {
+
         Queue<String> colors = new PriorityQueue<>();
         colors.add("Blue");
         colors.add("Black");
@@ -14,6 +16,7 @@ public class Main {
         Map<String, Integer> map = new HashMap<>();
         map.put("A",1);
         map.put("B", 2);
+        map.replace("A",3);
 
         System.out.println("from queue " + colors);
         System.out.println("from map " + map);
